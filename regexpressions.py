09d2ff97b84875d8cbd7dcb536ba217c0b71a32a
MilanaMoroz <morozmilana2009@gmail.com>
2023-10-10 23:42:30 +0300

    mongo db

---
+++ regexpressions.py
@@ -0,0 +1,43 @@
+# text = '125map  MaP'
+#
+# cont_map = 'map' in text
+# print(cont_map)
+#
+# cont_map = text.count('map')
+# print(cont_map)
+
+import re
+
+text = 'I would like to buy this map. Map is useful. mapping map. 0 - zero . 24. '
+
+# match = re.findall('map', text)
+# print(match)
+
+# match = re.findall('[Mm]ap', text)
+# print(match)
+
+# match = re.findall('^[Mm]ap', text)
+# print(match)
+
+# match = re.findall('^[Mm]ap$', text)
+# print(match)
+
+# match = re.findall('^[Mm]ap$', text)
+# print(match)
+
+# match = re.findall(r'm.p\d', text)
+# print(match)
+
+# match = re.findall(r'm.p\D', text)
+# print(match)
+
+# match = re.findall(r'5{2,}\d{2}', text)
+# print(match)
+
+# match = re.findall(r'\b[A-Z0-9]{1,}', text)
+# print(match)
+
+# match = re.findall(r'^[A-Z0-9]{3}p', text)
+# print(match)
+
+
